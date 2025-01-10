9f14e78f7f5df06ecda9ca047d955c53fa189fb2
Kiran121245 <giddalurikiransai1@gmail.com>
2025-01-10 12:59:52 +0530

    GET - /user & /feed

--- src/app.js
+++ src/app.js
@@ -6,6 +6,42 @@ const app = express(); // this is an instance of the express application
 
 app.use(express.json()); // express json middleware
 
+// Getting the user by email
+app.get("/user", async (req, res)=>{ 
+    // console.log(req.body.email);
+    const userEmail = req.body.email;
+    try{
+        const users = await User.find({email: userEmail});
+        // console.log(users);
+        if (users.length === 0){
+            res.status(404).send("User not found");
+        }else{
+            res.send(users);
+        }
+    }catch(err){
+        res.status(500).send("Something went wrong");
+    }
+    
+      
+});
+
+// Getting all the Users
+app.get("/users", async (req, res)=>{ 
+    try{
+        const users = await User.find({});
+        // console.log(users);
+        if (users.length === 0){
+            res.status(404).send("No Users in Database");
+        }else{
+            res.send(users);
+        }
+    }catch(err){
+        res.status(500).send("Something went wrong");
+    }
+    
+      
+});
+
 app.post("/signup",async (req, res) => { 
     const user = new User(req.body); //creating new instance of user model
 
